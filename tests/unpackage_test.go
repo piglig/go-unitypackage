293e24f765f091b40843c5bf92d366336428a56a
piglig <zhu1197437384@gmail.com>
2023-03-23 17:53:22 +0800

    test: TestPackageExtractWithLeadingDots unit test

--- tests/unpackage_test.go
+++ tests/unpackage_test.go
@@ -54,3 +54,42 @@ func TestPackageExtract(t *testing.T) {
 		t.Errorf("got %v, wanted %v", string(data), "testing")
 	}
 }
+
+func TestPackageExtractWithLeadingDots(t *testing.T) {
+	dir, err := os.MkdirTemp("", "tmp")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
+	unityPath := filepath.Join(".", "test-data", "testLeadingDots.unitypackage")
+
+	err = utils.UnPackage(unityPath, dir)
+	if err != nil {
+		t.Fatalf("Failed to unpackage unitypackage: %v", err)
+		return
+	}
+
+	got := isDir(dir)
+	want := true
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+
+	got = isDir(filepath.Join(dir, "Assets"))
+	want = true
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+
+	got = !isDir(filepath.Join(dir, "Assets", "test.txt"))
+	want = true
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+
+	data, err := os.ReadFile(filepath.Join(dir, "Assets", "test.txt"))
+	if string(data) != "testing" {
+		t.Errorf("got %v, wanted %v", string(data), "testing")
+	}
+}
