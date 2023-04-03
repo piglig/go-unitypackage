2447b91611ac47755f78868f6a294e2a94dd54e6
piglig <zhu1197437384@gmail.com>
2023-04-03 17:30:43 +0800

    feat: cmd

---
+++ tools/go-unitypackage/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/piglig/go-unitypackage/tools/go-unitypackage/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
--- tools/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/piglig/go-unitypackage/tools/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
